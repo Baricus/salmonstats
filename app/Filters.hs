0be19b04020c89c0a34880db6036929e069e5326
Baricus <31926270+Baricus@users.noreply.github.com>
2023-06-04 23:20:41 -0500

    filter rework; all And on command line now

--- app/Filters.hs
+++ app/Filters.hs
@@ -10,9 +10,6 @@ import Salmon
 
 import qualified Data.Map as M
 
-import Data.List.NonEmpty (NonEmpty)
-import qualified Data.List.NonEmpty as NE
-
 import Data.Text (Text)
 
 import Data.Time
@@ -23,40 +20,45 @@ import Data.Time
       TimeZone,
       parseTimeM,
       localTimeToUTC ) 
-import Data.Foldable1 (Foldable1(foldrMap1))
 
 data Pred = Player Text
           | Stage Text
           | TimeBefore UTCTime
           | TimeAfter UTCTime
+          | Any  -- always true
         deriving (Read, Show, Eq, Ord)
 
-data Join = U -- Union
-          | I -- Intersection
-        deriving (Read, Show, Eq, Ord, Bounded, Enum)
-
-
-data Filter = Filter Join Pred
+-- generic in the filter to allow for transformations!
+data Filter op = And (Filter op) (Filter op)
+               | Or (Filter op) (Filter op)
+               | P op
         deriving (Read, Show, Eq, Ord)
 
-type Data = [Filter]
+instance Functor Filter where
+    fmap :: (a -> b) -> Filter a -> Filter b
+    fmap f = \cases
+        (P v)     -> P $ f v
+        (And l r) -> And (fmap f l) (fmap f r)
+        (Or  l r) -> Or (fmap f l) (fmap f r)
 
-opts :: UTCTime -> TimeZone -> TimeLocale -> Parser [Filter]
+type Data = Filter Pred
+
+opts :: UTCTime -> TimeZone -> TimeLocale -> Parser (Filter Pred)
 opts UTCTime{utctDay=day} zone local = 
     -- this allows you to repeat multiple before and afters, which is not ideal
     -- but better than nothing
-    many $ asum
-    [ (option (Filter I . TimeBefore <$> maybeReader (parseTime))
+    buildAndfilter <$> many (asum
+    [ (option (TimeBefore <$> maybeReader (parseTime))
             (long "before" <> short 'b' <> metavar timeFMT <> help "Filter matches after this time"))
-    , (option (Filter I . TimeAfter <$> maybeReader (parseTime))
+    , (option (TimeAfter <$> maybeReader (parseTime))
             (long "after" <> short 'a' <> metavar timeFMT <> help "Filter matches before this time"))
-    , (Filter U . Player 
+    , (Player 
             <$> strOption 
                 (long "player" <> short 'p' <> metavar "PLAYER" <> help "Filter matches to ones with PLAYER"))
-    , (Filter U . Stage <$> strOption
+    , (Stage <$> strOption
                 (long "stage" <> short 's' <> metavar "STAGE" <> help "Filter matches to ones on STAGE"))
-    ]
-   where timeFMT = "[yyyy-]mm-ddThh:mm[:ss][.sss]"
+    ])
+   where timeFMT = "[[yyyy-]mm-dd]_hh[:mm[[:ss]]]"
          -- so many time formats...
          parseTime timeStr = 
             let (startYear, _, _) = toGregorian day
@@ -74,20 +76,25 @@ opts UTCTime{utctDay=day} zone local =
             , replaceYear startYear . localTimeToUTC zone <$> parseTimeM True local "%m-%d_%H" timeStr
             , replaceYear startYear . localTimeToUTC zone <$> parseTimeM True local "%m-%d" timeStr
             , replaceYear startYear . localTimeToUTC zone <$> parseTimeM True local "%m-%d" timeStr
-            -- if given everything, just convert
-            , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%dT%T" timeStr
-            , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%d_%T" timeStr
+            -- if given full day and partial/no time, replace missing info with 00:00:00 as needed
             , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%dT%R" timeStr
             , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%d_%R" timeStr
             , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%dT%H" timeStr
             , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%d_%H" timeStr
             , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%d" timeStr
             , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%d" timeStr
+            -- if given everything, just convert
+            , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%dT%T" timeStr
+            , localTimeToUTC zone <$> parseTimeM False local "%Y-%m-%d_%T" timeStr
             ]
         -- clips invalid values
          replaceYear year (UTCTime d t) = let (_, curMonth, curDay) = toGregorian d
                                   in UTCTime (fromGregorian year curMonth curDay) t
 
+-- takes a list of predicates and adds them
+-- into a single "anded" together clause
+buildAndfilter :: [Pred] -> Filter Pred
+buildAndfilter = foldr (\p f -> And (P p) f) (P Any)
 
 -- convert a Predicate to its implementation
 fromPred  :: Pred -> (Round -> Bool)
@@ -96,25 +103,15 @@ fromPred = \cases
     (Stage  name)  -> ((== name) . stage)
     (TimeBefore t) -> ((< t) . time)
     (TimeAfter t)  -> ((> t) . time)
+    (Any)          -> const True
 
-
--- folds a list of filters into a single filter instruction
-fromFilters :: NonEmpty Filter -> (Round -> Bool)
-fromFilters  l = foldrMap1 firstFilter foldFilter l
-    where -- all other filters hold and this one does too
-          foldFilter (Filter I p) f = (fromPred p `andF` f)
-          -- either P is true and all previous filters hold or P is false
-          -- and all previous filters hold
-          foldFilter (Filter U p) f = ((fromPred p `andF` f) `orF` f)
-
-          -- first filter must always hold, regardless of Union or Intersection
-          firstFilter (Filter _ p) = (\r -> fromPred p $ r)
-
-          andF f1 f2 = (\r -> f1 r && f2 r)
-          orF  f1 f2 = (\r -> f1 r || f2 r)
+fromFilters :: Filter (Round -> Bool) -> (Round -> Bool)
+fromFilters = \cases
+    (P p)     -> p
+    (And l r) -> liftA2 (&&) (fromFilters l) (fromFilters r)
+    (Or  l r) -> liftA2 (||) (fromFilters l) (fromFilters r)
 
 -- filters the roundMap given the list of filters to apply
 -- assumes that the empty list means no filter
-filterRounds :: [Filter] -> RoundMap -> RoundMap
-filterRounds [] = id
-filterRounds l  = M.filter . fromFilters . NE.fromList $ l -- safe due to earlier case for []
+filterRounds :: Filter Pred -> RoundMap -> RoundMap
+filterRounds = M.filter . fromFilters . fmap fromPred
